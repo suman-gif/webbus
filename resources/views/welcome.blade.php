f0059db4abbf5c33dcbf97bd7e0058b1619ca730
Manish Shah <shahmanish877@gmail.com>
2021-02-14 14:35:59 +0545

    Updated banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,29 +6,27 @@
 <div id="demo" class="carousel slide" data-ride="carousel"  style="margin-top: -50px;">
 
   <!-- Indicators -->
-  <ul class="carousel-indicators">
+<!--   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
     <li data-target="#demo" data-slide-to="1"></li>
-  </ul>
+  </ul> -->
   
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="{{ asset('/assets/images/banner1.jpg') }}"  width="1100" height="600">
+      <img src="{{ asset('/assets/images/banner2.jpg') }}"  width="1200" height="550">
     </div>
-    <div class="carousel-item">
-      <img src="{{ asset('/assets/images/banner2.png') }}"  width="1100" height="600">
-    </div>
+    
     
   </div>
   
   <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <!-- <a class="carousel-control-prev" href="#demo" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
   </a>
   <a class="carousel-control-next" href="#demo" data-slide="next">
     <span class="carousel-control-next-icon"></span>
-  </a>
+  </a> -->
 </div>
 
 
